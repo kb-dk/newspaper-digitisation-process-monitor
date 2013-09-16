ee3d93c6f3cd205042ad2a2635ed029782cc5fc1
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-09-16 14:50:30 +0200

    Most of the structure of the project

---
+++ process-monitor-datasource-interfaces/src/main/java/dk/statsbiblioteket/newspaper/processMonitor/datasources/DataSource.java
@@ -0,0 +1,11 @@
+package dk.statsbiblioteket.newspaper.processMonitor.datasources;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: abr
+ * Date: 9/16/13
+ * Time: 2:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DataSource {
+}
