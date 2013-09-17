9ab31c3547f6f15599ebca84787d58e8b464b53d
ktc <ktc@statsbiblioteket.dk>
2013-09-17 09:35:17 +0200

    Add javadoc for rest service, add details query param to methods (default to false)

--- process-monitor-backend-service/src/main/java/dk/statsbiblioteket/newpaper/processmonitor/backend/Batches.java
+++ process-monitor-backend-service/src/main/java/dk/statsbiblioteket/newpaper/processmonitor/backend/Batches.java
@@ -3,9 +3,18 @@ package dk.statsbiblioteket.newpaper.processmonitor.backend;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+
+/**
+ * Service class to expose retrieval of Batch (@see Batch) and Events (@see Event) objects for monitoring progress and state  
+ */
 
 @Path("/")
 public class Batches {
@@ -57,16 +66,29 @@ public class Batches {
         dummyBatches.add(b2);
     } 
     
-    
+    /**
+     * Retrieves a list of all known Batch objects (@see Batch).
+     * @param details If true, will also include the available details for each event in the Batch objects. Defaults to false.
+     * @return List<Batch> as JSON data.   
+     */
     @GET
     @Path("/")
-    public List<Batch> getBatches() {
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Batch> getBatches(@QueryParam("details") @DefaultValue("false") boolean details) {
         return dummyBatches;
     }
     
+    /**
+     * Retrieves a specific batch given it's ID. 
+     * @param batchID The ID of the specific batch
+     * @param details If true, will also include the available details for each event in the Batch. Defaults to false.
+     * @return Batch as JSON Object
+     */
     @GET
     @Path("{batchID}")
-    public Batch getSpecificBatch(@PathParam("batchID") String batchID) {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Batch getSpecificBatch(@PathParam("batchID") String batchID, 
+            @QueryParam("details") @DefaultValue("false") boolean details) {
         Batch batch = null;
         for(Batch b : dummyBatches) {
             if(b.getBatchID().equals(batchID)) {
@@ -76,9 +98,17 @@ public class Batches {
         return batch;
     }
     
+    /**
+     * Retrieves a specific Event for a specific Batch.
+     * @param batchID The ID of the specific batch
+     * @param eventID The ID of the specific event
+     * @param details If true, will also include the available details. Defaults to false.
+     */
     @GET
     @Path("{batchID}/{eventID}")
-    public Event getSpecificBatchEvent(@PathParam("batchID") String batchID, @PathParam("eventID") String eventID) {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Event getSpecificBatchEvent(@PathParam("batchID") String batchID, @PathParam("eventID") String eventID, 
+            @QueryParam("details") @DefaultValue("false") boolean details) {
         Event event = null;
         for(Batch b : dummyBatches) {
             if(b.getBatchID().equals(batchID)) {
