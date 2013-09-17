be9f77052a96ee62b68c176de7754bfcf7aee68a
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-09-17 16:26:05 +0200

    The framework for an integration test

---
+++ process-monitor-frontend/src/test/java/dk/statsbiblioteket/newspaper/processmonitor/frontend/BackendTest.java
@@ -0,0 +1,14 @@
+package dk.statsbiblioteket.newspaper.processmonitor.frontend;
+
+import org.testng.annotations.Test;
+
+
+public class BackendTest {
+
+    @Test(groups = "integrationTest")
+    public void testGetBatches() {
+        System.out.println("test of integration test");
+
+
+    }
+}
