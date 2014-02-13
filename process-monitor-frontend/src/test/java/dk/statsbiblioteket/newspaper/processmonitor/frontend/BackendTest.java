d94c4430aaa4f61ff1565797891cc1b1c0af8f8e
ktc <ktc@statsbiblioteket.dk>
2014-02-13 11:15:58 +0100

    update service path in testclass

--- process-monitor-frontend/src/test/java/dk/statsbiblioteket/newspaper/processmonitor/frontend/BackendTest.java
+++ process-monitor-frontend/src/test/java/dk/statsbiblioteket/newspaper/processmonitor/frontend/BackendTest.java
@@ -25,7 +25,7 @@ public class BackendTest {
     public void setup() {
         config = new DefaultClientConfig();
         config.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-        integrationTestServer = "http://localhost:8081/process-monitor-frontend/batches/";
+        integrationTestServer = "http://localhost:8081/process-monitor-frontend/services/batches/";
     }
 
 
