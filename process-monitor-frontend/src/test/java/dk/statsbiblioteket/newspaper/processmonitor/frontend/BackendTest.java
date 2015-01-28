6bb515f495751dfaa42b972532f204f06901dde8
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-01-28 14:31:32 +0100

    Tests updated to work

--- process-monitor-frontend/src/test/java/dk/statsbiblioteket/newspaper/processmonitor/frontend/BackendTest.java
+++ process-monitor-frontend/src/test/java/dk/statsbiblioteket/newspaper/processmonitor/frontend/BackendTest.java
@@ -13,29 +13,30 @@ import java.io.IOException;
 public class BackendTest {
 
     public static final String HEADER
-            = "Batch;Roundtrip;Avis id;Pages;Manually_stopped;Manually_stopped_timestamp;Manually_stopped_duration;Shipped_to_supplier;Shipped_to_supplier_timestamp;Shipped_to_supplier_duration;Data_Received;Data_Received_timestamp;Data_Received_duration;Metadata_Archived;Metadata_Archived_timestamp;Metadata_Archived_duration;Data_Archived;Data_Archived_timestamp;Data_Archived_duration;Structure_Checked;Structure_Checked_timestamp;Structure_Checked_duration;JPylyzed;JPylyzed_timestamp;JPylyzed_duration;Histogrammed;Histogrammed_timestamp;Histogrammed_duration;Metadata_checked;Metadata_checked_timestamp;Metadata_checked_duration;Manual_QA_Flagged;Manual_QA_Flagged_timestamp;Manual_QA_Flagged_duration;Roundtrip_Approved;Roundtrip_Approved_timestamp;Roundtrip_Approved_duration;Dissemination_Copy_Generated;Dissemination_Copy_Generated_timestamp;Dissemination_Copy_Generated_duration;Dissemination_Editions_Generated;Dissemination_Editions_Generated_timestamp;Dissemination_Editions_Generated_duration;Metadata_Enriched;Metadata_Enriched_timestamp;Metadata_Enriched_duration;Cleaned_lesser_roundtrips;Cleaned_lesser_roundtrips_timestamp;Cleaned_lesser_roundtrips_duration;Data_Released;Data_Released_timestamp;Data_Released_duration;Received_from_supplier;Received_from_supplier_timestamp;Received_from_supplier_duration";
+            = "Batch;Roundtrip;Avis id;Start date;End date;Pages;Manually_stopped;Manually_stopped_timestamp;Manually_stopped_duration;Shipped_to_supplier;Shipped_to_supplier_timestamp;Shipped_to_supplier_duration;Data_Received;Data_Received_timestamp;Data_Received_duration;Metadata_Archived;Metadata_Archived_timestamp;Metadata_Archived_duration;Data_Archived;Data_Archived_timestamp;Data_Archived_duration;Structure_Checked;Structure_Checked_timestamp;Structure_Checked_duration;JPylyzed;JPylyzed_timestamp;JPylyzed_duration;Histogrammed;Histogrammed_timestamp;Histogrammed_duration;Metadata_checked;Metadata_checked_timestamp;Metadata_checked_duration;Manual_QA_Flagged;Manual_QA_Flagged_timestamp;Manual_QA_Flagged_duration;Roundtrip_Approved;Roundtrip_Approved_timestamp;Roundtrip_Approved_duration;Dissemination_Copy_Generated;Dissemination_Copy_Generated_timestamp;Dissemination_Copy_Generated_duration;Dissemination_Editions_Generated;Dissemination_Editions_Generated_timestamp;Dissemination_Editions_Generated_duration;Metadata_Enriched;Metadata_Enriched_timestamp;Metadata_Enriched_duration;Cleaned_lesser_roundtrips;Cleaned_lesser_roundtrips_timestamp;Cleaned_lesser_roundtrips_duration;Data_Released;Data_Released_timestamp;Data_Released_duration;Received_from_supplier;Received_from_supplier_timestamp;Received_from_supplier_duration";
     public static final boolean enabled = true;
+    protected static final String JETTY_TEST = "jettyTest";
     private DefaultClientConfig config;
 
     private String batchID = "4001";
     private String eventID = "Shipped_to_supplier";
     private String integrationTestServer;
 
-    @BeforeClass(groups = "integrationTest", enabled = enabled)
+    @BeforeClass(groups = JETTY_TEST, enabled = enabled)
     public void setup() throws IOException {
         integrationTestServer = "http://localhost:8080/process-monitor-frontend/services/batches";
         config = new DefaultClientConfig();
     }
 
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetBatches() {
         System.out.println("Running testGetBatches");
         Batch[] result = Client.create(config).resource(integrationTestServer).get(Batch[].class);
         Assert.assertTrue(result.length > 1, "We expected more than one batch to be returned");
     }
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetSingleBatch() {
         System.out.println("Running testGetSingleBatch");
         Batch result = Client.create(config).resource(integrationTestServer).path(batchID + "").get(Batch.class);
@@ -46,7 +47,7 @@ public class BackendTest {
     }
 
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetSingleEvent() {
         System.out.println("Running testGetSingleEvent");
         Event result = Client.create(config)
@@ -61,7 +62,7 @@ public class BackendTest {
         Assert.assertTrue(result.isSuccess(), "The event is not succesful");
     }
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetBatchesCSV() {
         String result = Client.create(config)
                 .resource(integrationTestServer)
@@ -70,13 +71,13 @@ public class BackendTest {
         result = cleanDate(result);
         Assert.assertEquals(result.substring(0,result.indexOf('\n')), HEADER,
                           "Expect a column header first");
-        Assert.assertTrue(result.contains("\"=\"\"4001\"\"\";0;boersen;0;;;;true;1970-01-01 01:00:00;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n"),
+        Assert.assertTrue(result.contains("\"=\"\"4001\"\"\";0;boersen;;;0;;;;true;1970-01-01 01:00:00;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n"),
                           "The 4001 batch should be contained, but was " + result);
         Assert.assertTrue(result.contains("\"=\"\"400022028241\"\"\";1;"),
                           "The small test batch should be there, but was " + result);
     }
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetSingleBatchCSV() {
         String result = Client.create(config)
                 .resource(integrationTestServer)
@@ -86,14 +87,14 @@ public class BackendTest {
         result = cleanDate(result);
 
         Assert.assertEquals(result,
-                            HEADER + "\n" + "\"=\"\"4001\"\"\";0;boersen;0;;;;true;1970-01-01 01:00:00;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n");
+                            HEADER + "\n" + "\"=\"\"4001\"\"\";0;boersen;;;0;;;;true;1970-01-01 01:00:00;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n");
     }
 
     private String cleanDate(String result) {
         return result.replaceAll("[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}", "1970-01-01 01:00:00");
     }
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetSingleEventCSV() {
         String result = Client.create(config)
                 .resource(integrationTestServer)
@@ -106,6 +107,6 @@ public class BackendTest {
                 .accept("text/csv")
                 .get(String.class);
         result = cleanDate(result);
-        Assert.assertEquals(result, ";;;;true;1970-01-01 01:00:00;\n");
+        Assert.assertEquals(result, ";;;;;;true;1970-01-01 01:00:00;\n");
     }
 }
--- process-monitor-frontend/src/test/java/dk/statsbiblioteket/newspaper/processmonitor/frontend/BackendTest41Batch.java
+++ process-monitor-frontend/src/test/java/dk/statsbiblioteket/newspaper/processmonitor/frontend/BackendTest41Batch.java
@@ -13,7 +13,9 @@ import java.io.IOException;
 public class BackendTest41Batch {
 
     public static final String HEADER
-            = "Batch;Roundtrip;Avis id;Pages;Manually_stopped;Manually_stopped_timestamp;Manually_stopped_duration;Shipped_to_supplier;Shipped_to_supplier_timestamp;Shipped_to_supplier_duration;Data_Received;Data_Received_timestamp;Data_Received_duration;Metadata_Archived;Metadata_Archived_timestamp;Metadata_Archived_duration;Data_Archived;Data_Archived_timestamp;Data_Archived_duration;Structure_Checked;Structure_Checked_timestamp;Structure_Checked_duration;JPylyzed;JPylyzed_timestamp;JPylyzed_duration;Histogrammed;Histogrammed_timestamp;Histogrammed_duration;Metadata_checked;Metadata_checked_timestamp;Metadata_checked_duration;Manual_QA_Flagged;Manual_QA_Flagged_timestamp;Manual_QA_Flagged_duration;Roundtrip_Approved;Roundtrip_Approved_timestamp;Roundtrip_Approved_duration;Dissemination_Copy_Generated;Dissemination_Copy_Generated_timestamp;Dissemination_Copy_Generated_duration;Dissemination_Editions_Generated;Dissemination_Editions_Generated_timestamp;Dissemination_Editions_Generated_duration;Metadata_Enriched;Metadata_Enriched_timestamp;Metadata_Enriched_duration;Cleaned_lesser_roundtrips;Cleaned_lesser_roundtrips_timestamp;Cleaned_lesser_roundtrips_duration;Data_Released;Data_Released_timestamp;Data_Released_duration;Received_from_supplier;Received_from_supplier_timestamp;Received_from_supplier_duration";
+            = "Batch;Roundtrip;Avis id;Start date;End date;Pages;Manually_stopped;Manually_stopped_timestamp;Manually_stopped_duration;Shipped_to_supplier;Shipped_to_supplier_timestamp;Shipped_to_supplier_duration;Data_Received;Data_Received_timestamp;Data_Received_duration;Metadata_Archived;Metadata_Archived_timestamp;Metadata_Archived_duration;Data_Archived;Data_Archived_timestamp;Data_Archived_duration;Structure_Checked;Structure_Checked_timestamp;Structure_Checked_duration;JPylyzed;JPylyzed_timestamp;JPylyzed_duration;Histogrammed;Histogrammed_timestamp;Histogrammed_duration;Metadata_checked;Metadata_checked_timestamp;Metadata_checked_duration;Manual_QA_Flagged;Manual_QA_Flagged_timestamp;Manual_QA_Flagged_duration;Roundtrip_Approved;Roundtrip_Approved_timestamp;Roundtrip_Approved_duration;Dissemination_Copy_Generated;Dissemination_Copy_Generated_timestamp;Dissemination_Copy_Generated_duration;Dissemination_Editions_Generated;Dissemination_Editions_Generated_timestamp;Dissemination_Editions_Generated_duration;Metadata_Enriched;Metadata_Enriched_timestamp;Metadata_Enriched_duration;Cleaned_lesser_roundtrips;Cleaned_lesser_roundtrips_timestamp;Cleaned_lesser_roundtrips_duration;Data_Released;Data_Released_timestamp;Data_Released_duration;Received_from_supplier;Received_from_supplier_timestamp;Received_from_supplier_duration";
+
+    protected static final String JETTY_TEST = "jettyTest";
 
 
     public static final boolean enabled = true;
@@ -24,21 +26,21 @@ public class BackendTest41Batch {
     private String eventID = "Metadata_Archived";
     private String integrationTestServer;
 
-    @BeforeClass(groups = "integrationTest", enabled = enabled)
+    @BeforeClass(groups = JETTY_TEST, enabled = enabled)
     public void setup() throws IOException {
         integrationTestServer = "http://localhost:8080/process-monitor-frontend/services/batches";
         config = new DefaultClientConfig();
     }
 
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetBatches() {
         System.out.println("Running testGetBatches");
         Batch[] result = Client.create(config).resource(integrationTestServer).get(Batch[].class);
         Assert.assertTrue(result.length > 1, "We expected more than one batch to be returned");
     }
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetSingleBatch() {
         System.out.println("Running testGetSingleBatch");
         Batch result = Client.create(config).resource(integrationTestServer).path(batchID + "").get(Batch.class);
@@ -49,7 +51,7 @@ public class BackendTest41Batch {
     }
 
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetSingleEvent() {
         System.out.println("Running testGetSingleEvent");
         Event result = Client.create(config)
@@ -64,7 +66,7 @@ public class BackendTest41Batch {
         Assert.assertTrue(result.isSuccess(), "The event is not succesful");
     }
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetBatchesCSV() {
         String result = Client.create(config)
                 .resource(integrationTestServer)
@@ -74,13 +76,13 @@ public class BackendTest41Batch {
 
         Assert.assertEquals(result.substring(0,result.indexOf('\n')), HEADER,
                           "Expect a column header first");
-        Assert.assertTrue(result.contains("\"=\"\"4001\"\"\";0;boersen;0;;;;true;1970-01-01 01:00:00;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n"),
+        Assert.assertTrue(result.contains("\"=\"\"4001\"\"\";0;boersen;;;0;;;;true;1970-01-01 01:00:00;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n"),
                           "The 4001 batch should be contained, but was " + result);
         Assert.assertTrue(result.contains("\"=\"\"400022028241\"\"\";1;"),
                           "The small test batch should be there, but was " + result);
     }
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetSingleBatchCSV() {
         String result = Client.create(config)
                 .resource(integrationTestServer)
@@ -88,15 +90,17 @@ public class BackendTest41Batch {
                 .accept("text/csv")
                 .get(String.class);
         result = cleanDate(result);
-        Assert.assertEquals(result,
-                            HEADER+"\n" + "\"=\"\"400022028241\"\"\";1;adresseavisen1759;9;;;;true;1970-01-01 01:00:00;;true;1970-01-01 01:00:00;;true;1970-01-01 01:00:00;P0Y0M0DT0H0M3.817S;true;1970-01-01 01:00:00;P0Y0M0DT0H0M12.257S;true;1970-01-01 01:00:00;P0Y0M0DT0H0M2.940S;true;1970-01-01 01:00:00;P0Y0M0DT0H0M40.401S;true;1970-01-01 01:00:00;P0Y0M0DT0H0M40.988S;true;1970-01-01 01:00:00;P0Y0M0DT0H0M8.812S;true;1970-01-01 01:00:00;P0Y0M0DT0H0M3.145S;;;;;;;;;;;;;;;;;;;;;\n");
+        Assert.assertEquals(result.trim(),
+                            HEADER+"\n" + "\"=\"\"400022028241\"\"\";1;adresseavisen1759;1795-06-01;1795-06-15;9;;;;true;1970-01-01 01:00:00;;true;1970-01-01 01:00:00;;true;1970-01-01 01:00:00;P0Y1M0DT0H0M0.101S;true;1970-01-01 01:00:00;P0Y1M0DT0H0M0.101S;true;1970-01-01 01:00:00;P0Y1M0DT0H0M0.101S;true;1970-01-01 01:00:00;P0Y1M0DT0H0M0.101S;true;1970-01-01 01:00:00;P0Y1M0DT0H0M0.101S;true;1970-01-01 01:00:00;P0Y1M0DT0H0M0.101S;true;1970-01-01 01:00:00;P0Y1M0DT0H0M0.101S;;;;;;;;;;;;;;;;;;;;;");
     }
 
     private String cleanDate(String result) {
-        return result.replaceAll("[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}", "1970-01-01 01:00:00");
+        result =  result.replaceAll("[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}", "1970-01-01 01:00:00");
+        result = result.replaceAll("P\\d+Y\\d+M\\d+DT\\d+H\\d+M\\d+\\.\\d+S", "P0Y1M0DT0H0M0.101S");
+        return result;
     }
 
-    @Test(groups = "integrationTest", enabled = enabled)
+    @Test(groups = JETTY_TEST, enabled = enabled)
     public void testGetSingleEventCSV() {
         String result = Client.create(config)
                 .resource(integrationTestServer)
@@ -109,6 +113,6 @@ public class BackendTest41Batch {
                 .accept("text/csv")
                 .get(String.class);
         result = cleanDate(result);
-        Assert.assertEquals(result, ";;;;true;1970-01-01 01:00:00;\n");
+        Assert.assertEquals(result, ";;;;;;true;1970-01-01 01:00:00;\n");
     }
 }
